fc884f8f40a6429db8c96a4f0891cebbd9992c54
Robert Frazier <robert.v.frazier@gmail.com>
2017-11-29 15:39:36 -0500

    Now populating the database correctly.

--- server.js
+++ server.js
@@ -159,4 +159,4 @@ if (require.main === module) {
   runServer().catch(err => console.error(err));
 };
 
-module.exports = {runServer, app, closeServer};
+module.exports = {runServer, app, closeServer,};
--- test.js
+++ test.js
@@ -4,20 +4,28 @@ const mongoose = require('mongoose');
 const {TEST_DATABASE_URL, PORT} = require('./config');
 const {BlogPost} = require('./models');
 const {app, runServer, closeServer} = require('./server');
+const seedData = require('./seed-data.json');
 const mocha = require('mocha');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
 chai.use(chaiHttp);
 
+function tearDownDb() {
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
+}
+
 beforeEach(function(){
-  console.log('Seeding data . . .');
-  const seedData = [{
-    author: 'James Smith',
-    content: 'Blog goes here.',
-    title: 'A Blog Post'
-  }];
   return BlogPost.insertMany(seedData);
+});
+
+afterEach(function() {
+  return tearDownDb();
 });
 
 before(function() {
@@ -30,20 +38,41 @@ after(function() {
 
 describe('Get all posts.', function(){
   it('Should get all posts.', function(){
-  	let apiRes;
+    let apiRes;
     return chai.request(app)
       .get('/posts')
       .then((res)=>{
-      	apiRes=res;
+        apiRes=res;
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.a('array');
         res.body.length.should.be.above(0);
         const myId=res.body[0].id;
         console.log(myId);
-        return BlogPost.findById(myId)
+        return BlogPost.findById(myId);
       }).then((myBlogPost)=>{
-      	myBlogPost.title.should.equal(apiRes.body[0].title);
+        myBlogPost.title.should.equal(apiRes.body[0].title);
       }); 		
   });
 });
+
+describe('Get one post.', function(){
+  it('Should get one post.', function(){
+    let apiRes;
+    return chai.request(app)
+      .get('/posts/:id')
+      .then((res)=>{
+        apiRes=res;
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.be.above(0);
+        const myId=res.body[0].id;
+        console.log(myId);
+        return BlogPost.findById(myId);
+      }).then((myBlogPost)=>{
+        myBlogPost.title.should.equal(apiRes.body[0].title);
+      }); 		
+  });
+});
+
